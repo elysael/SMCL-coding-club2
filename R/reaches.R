d327a56d6f0407b8d8705306599b7b72daefdb36
elysael <elysaelio@gmail.com>
2025-02-16 23:46:54 -0500

    added new code

--- R/reaches.R
+++ R/reaches.R
@@ -7,16 +7,36 @@ getParticipantTraining <- function(group, participant) {
                     stringsAsFactors = F)
   RO_df <- read.csv(file=RO_file, 
                     stringsAsFactors = F)
+  
+  return(list('aligned' = AL_df, 
+              'rotated' = RO_df))
 }
 
 
 getGroupTraining <- function(group) {
   
   participants <- groupParticipants(group = group)
+  print(participants)
   
-  for (participant in participants) {
+  for (participant in participants[c(1:3)]) {
     
     participant_df <- getParticipantTraining(group = group, participant = participant)
+  
+    
+    baseline <- getBaseline(df = participant_df[['aligned']])
   }
   
 }
+
+
+getBaseline <- function(df) {
+  df <- df[which(df$trial_num %in% c(31:45)),]
+  str(df)
+  
+  #rialnos <-
+  
+  
+  
+  
+}
+
